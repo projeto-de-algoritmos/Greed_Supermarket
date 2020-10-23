fa500b38d1625993db9541cb5e51b6de5f0263f9
Lucas Gomes <lucas.gomesgs0@gmail.com>
2020-10-23 04:35:54 -0300

    Adding ui

---
+++ supermarket.py
@@ -0,0 +1,25 @@
+class Supermarket():
+    total = 0.0
+
+    def get_total(self):
+        return self.total
+
+    def change_value(self, value):
+        self.total += value
+
+    def findMin(self, change):
+        currency = [0.01, 0.05, 0.1, 0.25, 0.5, 1.00, 2.0, 5.0, 10.0, 20.0, 50.0, 100.0, 200.0]
+
+        n = len(currency)
+        result = []
+        i = n - 1
+        change = round(change, 2)
+
+        while i >= 0:
+            while change >= currency[i]:
+                change = round(change - currency[i],2)
+                result.append(currency[i])
+
+            i -= 1
+        
+        return result
